695911f57335b02dc592d84d9d2ae437efc0036e
tellnobody1 <6303577+tellnobody1@users.noreply.github.com>
2024-02-18 19:54:38 +0200

    fix board display

--- app/src/main/kotlin/com/mukeshsolanki/snake/presentation/component/Board.kt
+++ app/src/main/kotlin/com/mukeshsolanki/snake/presentation/component/Board.kt
@@ -1,43 +1,53 @@
 package com.mukeshsolanki.snake.presentation.component
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import com.mukeshsolanki.snake.data.model.State
-import com.mukeshsolanki.snake.domain.game.GameEngine.Companion.BOARD_WIDTH
 import com.mukeshsolanki.snake.presentation.theme.DarkGreen
-import kotlin.math.floor
+import com.mukeshsolanki.snake.presentation.theme.border2dp
 
 @Composable
 fun Board(state: State, onSizeInit: (Int, Int) -> Unit) {
-    BoxWithConstraints {
-        val tileSize = with(LocalDensity.current) {
-            floor(maxWidth.toPx() / BOARD_WIDTH).dp
-        }
-        with(LocalDensity.current) {
-            val w = maxWidth.toPx()
-            val h = maxHeight.toPx()
-            val s = tileSize.toPx()
-            onSizeInit(floor(w / s).toInt(), floor(h / s).toInt())
-        }
-        Box(Modifier.size(maxWidth, maxHeight))
-        Box(
-            Modifier
-                .offset(x = tileSize * state.food.first, y = tileSize * state.food.second)
-                .size(tileSize)
-                .background(DarkGreen, CircleShape)
-        )
-        state.snake.forEach {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center) {
+        BoxWithConstraints {
+            val tileSize = 16.dp
+            val w = ((maxWidth - border2dp) / tileSize).toInt()
+            val h = ((maxHeight - border2dp) / tileSize).toInt()
+            onSizeInit(w, h)
             Box(
-                modifier = Modifier
-                    .offset(x = tileSize * it.first, y = tileSize * it.second)
-                    .size(tileSize)
-                    .background(DarkGreen)
+                Modifier
+                    .size(tileSize * w + border2dp, tileSize * h + border2dp)
+                    .border(border2dp, DarkGreen)
             )
+            Box(
+                Modifier
+                    .offset(x = tileSize * state.food.first, y = tileSize * state.food.second)
+                    .size(tileSize)
+                    .background(DarkGreen, CircleShape)
+            )
+            state.snake.forEach {
+                Box(
+                    modifier = Modifier
+                        .offset(x = tileSize * it.first, y = tileSize * it.second)
+                        .size(tileSize)
+                        .background(DarkGreen)
+                )
+            }
         }
     }
 }
